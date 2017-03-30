f812fd8d1bb321d7cab357e2b99922dc11e5fd6c
Javed Qadrud-Din <jqjunk@MacBook-Pro-3.local>
2017-03-30 11:45:29 -0600

    add support for tags across 2 lines

--- detagger.py
+++ detagger.py
@@ -6,10 +6,15 @@ r = open(sys.argv[1], 'r')
 w = open(sys.argv[1] + '_detagged.txt', 'w')
 
 regex = re.compile(r'<.*?>')
+regex_tag_to_end_of_line = re.compile(r'<.*?\n')
+regex_start_of_line_to_tag = re.compile(r'^.*?>')
 
 
 for line in r:
-    w.write(regex.sub(' ', line))
+    line = regex.sub(' ', line)
+    line = regex_tag_to_end_of_line.sub('', line)
+    line = regex_start_of_line_to_tag.sub('',line)
+    w.write(line)
 
 w.close()
 r.close()
